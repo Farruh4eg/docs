2254fca6b45197e196bc80fbd3daf139b661cc31
farruh4eg <farruh4eg@gmail.com>
2024-12-25 22:26:45 +0300

    be the first

---
+++ src/routes/v1/docs/+server.ts
@@ -0,0 +1,17 @@
+import type { RequestHandler } from '@sveltejs/kit';
+import fs from 'node:fs/promises';
+
+const prefix = './src/routes/v1/docs/files';
+
+export const GET: RequestHandler = async ({ url }) => {
+	let query = url.searchParams.get('q') || '';
+
+	try {
+		const fileContent = await fs.readFile(`${prefix}/${query}.txt`, { encoding: 'utf-8' });
+		return new Response(fileContent);
+	} catch (e) {
+		console.log(e);
+	}
+
+	return new Response();
+};
