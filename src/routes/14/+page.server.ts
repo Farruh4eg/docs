e709dc0e26f4d4b7d0dbc24d9cab348f0937f09c
farruh4eg <farruh4eg@gmail.com>
2024-12-26 01:52:23 +0300

    add 14

--- src/routes/14/+page.server.ts
+++ src/routes/14/+page.server.ts
@@ -1,374 +1,198 @@
 export const load = async() => {
-  let data = `\n\n\nMAINPAGE.XAML
-<?xml version="1.0" encoding="utf-8" ?>
+  let data = `\n\n\nMAINPAGE.XAML 
+         
+          <?xml version="1.0" encoding="utf-8" ?>
 <ContentPage xmlns="http://schemas.microsoft.com/dotnet/2021/maui"
              xmlns:x="http://schemas.microsoft.com/winfx/2009/xaml"
-             x:Class="DoctorApp.MainPage">
+             x:Class="resheto.MainPage">
 
     <ScrollView>
         <VerticalStackLayout
             Padding="30,0"
             Spacing="25">
-            <Image
-                Source="dotnet_bot.png"
-                HeightRequest="185"
-                Aspect="AspectFit"
-                SemanticProperties.Description="dot net bot in a hovercraft number nine" />
-
-            <Label
-                Text="Hello, World!"
-                Style="{StaticResource Headline}"
-                SemanticProperties.HeadingLevel="Level1" />
-
-            <Label
-                Text="Welcome to &#10;.NET Multi-platform App UI"
-                Style="{StaticResource SubHeadline}"
-                SemanticProperties.HeadingLevel="Level2"
-                SemanticProperties.Description="Welcome to dot net Multi platform App U I" />
-
-            <Button
-                x:Name="CounterBtn"
-                Text="Click me" 
-                SemanticProperties.Hint="Counts the number of times you click"
-                Clicked="OnCounterClicked"
-                HorizontalOptions="Fill" />
+            <Label Text="Введите число n:"/>
+            <Entry x:Name="Num" Placeholder="Введите число здесь..." Keyboard="Numeric"/>
+            <Button x:Name="Start" Text="Enter" Clicked="Start_Clicked"/>
+            <Grid x:Name="ResultResh" Grid.Row="1" ColumnSpacing="10" RowSpacing="10" Padding="10"/>
+            <Button x:Name="Next" Text="Следующая итерация" Clicked="Button_Clicked"
+            IsVisible="{OnPlatform
+                            x:Boolean,
+                            Android=false,
+                            WinUI=true}"/>
         </VerticalStackLayout>
     </ScrollView>
 
 </ContentPage>
 
-----------------------------------------------------------------------------
+	MAINPAGE.XAML.CS
 
-DoctorsPage.xaml
-<ContentPage xmlns="http://schemas.microsoft.com/dotnet/2021/maui"
-             xmlns:x="http://schemas.microsoft.com/winfx/2009/xaml"
-             x:Class="DoctorApp.DoctorsPage"
-             Title="Выберите врача">
-    <StackLayout Padding="20">
+using System.Collections.Generic;
 
-        <Label Text="Список врачей"
-               FontSize="20"
-               HorizontalOptions="Center"
-               Margin="0,10,0,20"/>
-
-        <ListView x:Name="DoctorsListView"
-                  ItemSelected="OnDoctorSelected"
-                  BackgroundColor="#F0F0F0"
-                  SeparatorColor="Gray">
-            <ListView.ItemTemplate>
-                <DataTemplate>
-                    <ViewCell>
-                        <StackLayout Padding="10" Orientation="Horizontal">
-                            <Label Text="{Binding Name}" FontSize="16" VerticalOptions="Center"/>
-                            <Label Text="{Binding Specialty}" FontSize="14" VerticalOptions="Center" TextColor="Gray"/>
-                        </StackLayout>
-                    </ViewCell>
-                </DataTemplate>
-            </ListView.ItemTemplate>
-        </ListView>
-    </StackLayout>
-</ContentPage>
-
-
-----------------------------------------------------------------------------
-DoctorsPage.xaml.cs
-namespace DoctorApp;
-
-public partial class DoctorsPage : ContentPage
+namespace resheto
 {
-    public List<Doctor> Doctors { get; set; }
-
-    public DoctorsPage(Clinic clinic)
+    public partial class MainPage : ContentPage
     {
-        InitializeComponent();
-        Doctors = new List<Doctor>
-            {
-                new Doctor { Id = 1, Name = "Иван Иванов", Specialty = "Терапевт" },
-                new Doctor { Id = 2, Name = "Петр Петров", Specialty = "Хирург" },
-                new Doctor { Id = 3, Name = "Мария Сидорова", Specialty = "Окулист" }
-            };
-        DoctorsListView.ItemsSource = Doctors;
-    }
-
-    private async void OnDoctorSelected(object sender, SelectedItemChangedEventArgs e)
-    {
-        if (e.SelectedItem is Doctor selectedDoctor)
-        {
-            await Navigation.PushAsync(new AppointmentPage(selectedDoctor));
-        }
-    }
-}
-
-public class Doctor
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Specialty { get; set; }
-}
-
-----------------------------------------------------------------------------
-ClinicsPage.xaml
-<ContentPage xmlns="http://schemas.microsoft.com/dotnet/2021/maui"
-             xmlns:x="http://schemas.microsoft.com/winfx/2009/xaml"
-             x:Class="DoctorApp.ClinicsPage"
-             Title="Выберите поликлинику">
-    <StackLayout Padding="20">
-        <!-- Заголовок -->
-        <Label Text="Список поликлиник"
-               FontSize="20"
-               HorizontalOptions="Center"
-               Margin="0,10,0,20"/>
-
-        <!-- Список поликлиник -->
-        <ListView x:Name="ClinicsListView"
-                  ItemSelected="OnClinicSelected"
-                  BackgroundColor="#F0F0F0"
-                  SeparatorColor="Gray">
-            <ListView.ItemTemplate>
-                <DataTemplate>
-                    <ViewCell>
-                        <StackLayout Padding="10" Orientation="Horizontal">
-                            <Label Text="{Binding Name}" FontSize="16" VerticalOptions="Center"/>
-                        </StackLayout>
-                    </ViewCell>
-                </DataTemplate>
-            </ListView.ItemTemplate>
-        </ListView>
-    </StackLayout>
-</ContentPage>
-
-----------------------------------------------------------------------------
-ClinicsPage.xaml.cs
-namespace DoctorApp;
-
-public partial class ClinicsPage : ContentPage
-{
-    public List<Clinic> Clinics { get; set; }
-
-    public ClinicsPage()
-    {
-        InitializeComponent();
-        Clinics = new List<Clinic>
-        {
-            new Clinic { Id = 1, Name = "Поликлиника №1" },
-            new Clinic { Id = 2, Name = "Поликлиника №2" },
-            new Clinic { Id = 2, Name = "Поликлиника №3" },
-            new Clinic { Id = 2, Name = "Поликлиника №4" },
-        };
-        ClinicsListView.ItemsSource = Clinics;
-    }
-
-    private async void OnClinicSelected(object sender, SelectedItemChangedEventArgs e)
-    {
-        if (e.SelectedItem is Clinic selectedClinic)
-        {
-            await Navigation.PushAsync(new DoctorsPage(selectedClinic));
-        }
-    }
-}
-
-public class Clinic
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-}
-
-----------------------------------------------------------------------------
-APPSHELL.XAML
-<Shell xmlns="http://schemas.microsoft.com/dotnet/2021/maui"
-       xmlns:x="http://schemas.microsoft.com/winfx/2009/xaml"
-       xmlns:local="clr-namespace:DoctorApp"
-       x:Class="DoctorApp.AppShell"
-       Title="Запись к врачу">
-
-    <ShellContent
-        Title="Поликлиники"
-        ContentTemplate="{DataTemplate local:ClinicsPage}"
-        Route="ClinicsPage" />
-
-    <ShellContent
-        Title="Врачи"
-        ContentTemplate="{DataTemplate local:DoctorsPage}"
-        Route="DoctorsPage" />
-
-    <ShellContent
-        Title="Запись на прием"
-        ContentTemplate="{DataTemplate local:AppointmentPage}"
-        Route="AppointmentPage" />
-
-</Shell>
-
-----------------------------------------------------------------------------
-APPSHELL.XAML.CS
-using Microsoft.Maui;
-using Microsoft.Maui.Controls;
-
-namespace DoctorApp
-{
-    public partial class AppShell : Shell
-    {
-        public AppShell()
+        int count = 0;
+        public string res = "";
+        public MainPage()
         {
             InitializeComponent();
+        }
 
-            Routing.RegisterRoute("ClinicsPage", typeof(ClinicsPage));
-            Routing.RegisterRoute("DoctorsPage", typeof(DoctorsPage));
-            Routing.RegisterRoute("AppointmentPage", typeof(AppointmentPage));
+        private void Start_Clicked(object sender, EventArgs e)
+        {
+            if (int.TryParse(Num.Text, out int limit))
+            {
+                Resheto(limit);
+                count = limit;
+            }
+            else
+            {
+                ResultResh.Clear();
+                Label error = new Label { Text = "Invalid input" };
+                ResultResh.Add(error);
+                res = error.Text;
+            }
+        }
+
+        public void Resheto(int limit)
+        {
+            ResultResh.Clear();
+            if (limit < 2)
+            {
+                Label error = new Label { Text = "Limit must be greater than 1" };
+                ResultResh.Add(error);
+                res = error.Text;
+                return;
+            }
+
+
+            bool[] isPrime = new bool[limit + 1];
+            for (int i = 2; i <= limit; i++)
+            {
+                isPrime[i] = true;
+            }
+
+            for (int p = 2; p * p <= limit; p++)
+            {
+                if (isPrime[p])
+                {
+                    for (int i = p * p; i <= limit; i += p)
+                    {
+                        isPrime[i] = false;
+                    }
+                }
+            }
+            int col = 0;
+            int row = 0;
+            for (int i = 2; i <= limit; i++)
+            {
+                if (isPrime[i])
+                {
+                    Label primeLabel = new Label { Text = i.ToString() };
+                    ResultResh.Add(primeLabel, col, row);
+                    res = primeLabel.Text;
+                    col++;
+                    if (col > 5)
+                    {
+                        col = 0;
+                        row++;
+                    }
+
+                }
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (count != 0)
+            {
+                count++;
+                Resheto(count);
+                Num.Text = count.ToString();
+            } else
+            {
+                ResultResh.Clear();
+                Label error = new Label { Text = "Invalid input" };
+                ResultResh.Add(error);
+            }
         }
     }
-}
 
-----------------------------------------------------------------------------
-AppointmentPageTests.cs
-using System;
+}
+DECSTOP   
+	
+	using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace DoctorApp.Tests
+namespace SieveOfEratosthenesAppWinForms
 {
-    public class AppointmentPageTests
+    public partial class MainForm : Form
     {
-        [Fact]
-        public void CanCreateAppointment()
+        public MainForm()
         {
-            // Arrange
-            var appointmentPage = new AppointmentPage(new Doctor
-            {
-                Id = 1,
-                Name = "Иван Иванов",
-                Specialty = "Терапевт"
-            });
-
-            // Act
-            var appointment = new Appointment
-            {
-                Doctor = new Doctor { Id = 1, Name = "Иван Иванов", Specialty = "Терапевт" },
-                Date = DateTime.Now.Date,
-                Time = new TimeSpan(10, 0, 0),
-                PatientName = "Иван Петров",
-                PatientPhone = "1234567890"
-            };
-
-            // Assert
-            Assert.NotNull(appointment);
-            Assert.Equal("Иван Петров", appointment.PatientName);
-            Assert.Equal("1234567890", appointment.PatientPhone);
+            InitializeComponent();
         }
-    }
 
-    public class Appointment
-    {
-        public Doctor Doctor { get; set; }
-        public DateTime Date { get; set; }
-        public TimeSpan Time { get; set; }
-        public string PatientName { get; set; }
-        public string PatientPhone { get; set; }
-    }
-
-    public class AppointmentPage
-    {
-        private Doctor _selectedDoctor;
-
-        public AppointmentPage(Doctor doctor)
+        // Обработчик нажатия кнопки "Сформировать решето"
+        private void generateButton_Click(object sender, EventArgs e)
         {
-            _selectedDoctor = doctor;
+            if (!int.TryParse(numberTextBox.Text, out int number) || number < 2)
+            {
+                MessageBox.Show("Введите число больше 1.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var primes = GenerateSieve(number);
+            resultLabel.Text = string.Join(", ", primes);
+        }
+
+        // Метод для генерации решета Эратосфена
+        private List<int> GenerateSieve(int number)
+        {
+            var isPrime = new bool[number + 1];
+            for (int i = 2; i <= number; i++)
+            {
+                isPrime[i] = true;
+            }
+
+            for (int p = 2; p * p <= number; p++)
+            {
+                if (isPrime[p])
+                {
+                    for (int i = p * p; i <= number; i += p)
+                    {
+                        isPrime[i] = false;
+                    }
+                }
+            }
+
+            var primes = new List<int>();
+            for (int i = 2; i <= number; i++)
+            {
+                if (isPrime[i])
+                {
+                    primes.Add(i);
+                }
+            }
+
+            return primes;
         }
     }
 }
 
+Как запустить:
+1.Для MAUI:
+oСоздайте новый проект MAUI в Visual Studio.
+oЗамените содержимое файлов MainPage.xaml и MainPage.xaml.cs на приведенный выше код.
+oЗапустите приложение.
+2.Для Windows Forms:
+oСоздайте новый проект Windows Forms в Visual Studio.
+oДобавьте элементы управления (TextBox, Button, Label) на форму.
+oЗамените код на приведенный выше.
+oЗапустите приложение.
 
-----------------------------------------------------------------------------
-ClinicsPageTests.cs
-using System.Collections.Generic;
-using Xunit;
-
-namespace DoctorApp.Tests
-{
-    public class ClinicsPageTests
-    {
-        [Fact]
-        public void CanSelectClinicFromList()
-        {
-            // Arrange
-            var clinicsPage = new ClinicsPage();
-            clinicsPage.Clinics = new List<Clinic>
-            {
-                new Clinic { Id = 1, Name = "Поликлиника №1" },
-                new Clinic { Id = 2, Name = "Поликлиника №2" }
-            };
-
-            // Act
-            var selectedClinic = clinicsPage.Clinics[0];
-
-            // Assert
-            Assert.NotNull(selectedClinic);
-            Assert.Equal("Поликлиника №1", selectedClinic.Name);
-        }
-    }
-    public class Clinic
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-
-    public class ClinicsPage
-    {
-        public List<Clinic> Clinics { get; set; }
-    }
-}
-
-----------------------------------------------------------------------------
-DoctorsPageTests.cs
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DoctorApp.Tests
-{
-    public class DoctorsPageTests
-    {
-        [Fact]
-        public void CanSelectDoctorFromList()
-        {
-            // Arrange
-            var doctorsPage = new DoctorsPage(new Clinic { Id = 1, Name = "Поликлиника №1" });
-            doctorsPage.Doctors = new List<Doctor>
-            {
-                new Doctor { Id = 1, Name = "Иван Иванов", Specialty = "Терапевт" },
-                new Doctor { Id = 2, Name = "Петр Петров", Specialty = "Хирург" }
-            };
-
-            // Act
-            var selectedDoctor = doctorsPage.Doctors[1];
-
-            // Assert
-            Assert.NotNull(selectedDoctor);
-            Assert.Equal("Петр Петров", selectedDoctor.Name);
-            Assert.Equal("Хирург", selectedDoctor.Specialty);
-        }
-    }
-    public class Doctor
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Specialty { get; set; }
-    }
-
-    public class DoctorsPage
-    {
-        public List<Doctor> Doctors { get; set; }
-
-        public DoctorsPage(Clinic clinic)
-        {
-
-        }
-    }
-}
 
 \n\n\n\n\n\n\n\n\n`
 
